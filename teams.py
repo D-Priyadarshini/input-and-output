d944df6fbadad53776965073ebbc2e13c7ee14ff
Priyadarshini <145951339+D-Priyadarshini@users.noreply.github.com>
2024-09-30 10:28:31 +0530

    Create teams.py

---
+++ teams.py
@@ -0,0 +1,47 @@
+'''
+It was in the 1997-98 season that Ranchi saw the rise of the Captain Cool in the interschool trophy between DAV Jawahar Vidhya Mandir and Kendriya School. It was in that match Dhoni convinced Banerjee to be the opener and justified it with a double century.
+Write a program to display the details of the match with Team name, Scores of the team and Overs played.
+Input and Output Format:  
+Refer sample input and output for formatting specifications.
+[All text in bold corresponds to input and the rest corresponds to output]
+Sample Input and Output:
+Team 1:
+Team Name:
+DAV Jawahar Vidhya Mandir
+Score:
+150
+Overs played:
+20
+Team 2:
+Team name:
+Kendriya School
+Score:
+110
+Overs played:
+18
+Match Details:
+Team 1:
+Name: DAV Jawahar Vidhya Mandir
+Score: 150
+Overs played: 20
+Team 2:
+Name:  Kendriya School
+Score: 110
+Overs played: 18
+'''
+
+team1Name = input()
+team1Scorce = int(input())
+team1oversPlayed = int(input())
+team2Name = input()
+team2Scorce = int(input())
+team2oversPlayed = int(input())
+print("Match Details:")
+print("Team 1:")
+print("Team Name:",team1Name )
+print("Score:",team1Scorce)
+print("Overs played:",team1oversPlayed )
+print("Team 2:")
+print("Team Name:",team2Name )
+print("Score:",team2Scorce)
+print("Overs played:",team2oversPlayed )
